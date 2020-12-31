54d5cdb2065ebac0c88773255e73480f3d815951
swu3025 <65374120+swu3025@users.noreply.github.com>
2020-12-31 11:59:17 -0800

    Update and rename Part1/CellDivide.java to CellDivide.java

--- CellDivide.java
+++ CellDivide.java
@@ -6,8 +6,8 @@
  * Sources used: None
  *
  * the purpose of this file is to create a program that provides information
- * when the cell is being divided. It also has functions that provide
- * information on its string representation and apoptosis
+ * when the cell is being divided. It also has multiple functions that relate
+ * to the cell and it functionality.
  */
 
 import java.util.List;
@@ -15,16 +15,23 @@ import java.util.List;
 /**
  * The purpose of this class is to store/initialize the information of a
  * cell that is being divided, such as its rows, columns, mass, and the
- * direction the cell will divide into while also being able to get the
- * string representation and check the apoptosis of the cell
+ * direction the cell will divide into while also having functions that
+ * relate to the cells functionality.
  */
-public class CellDivide extends Cell{
+public class CellDivide extends Cell implements Divisible{
 
     // variable to store the string representation of the cell being divided
     private static final String CELLDIVIDE_STRING_REPRESENTATION = "+";
 
     //the condition of the checkApoptosis method
     private static final int CHECKAPOPTOSIS_CONDITION = 3;
+
+    /*the position of the cell in the method GetDivision() based on
+    the direction*/
+    private static final int DOWN = 0;
+    private static final int UP = 1;
+    private static final int LEFT = 2;
+    private static final int RIGHT = 3;
 
     //the direction the cell will divide to
     public int direction;
@@ -73,5 +80,44 @@ public class CellDivide extends Cell{
 
         //checks if the cell has exactly three neighbors
         return neighbors.size() == CHECKAPOPTOSIS_CONDITION;
+    }
+
+    /**
+     * The method creates a deep copy of the calling CellDivide object
+     *
+     * @return a Cell that is the deep copy of the CellDivide object
+     */
+    public Cell newCellCopy(){
+        CellDivide copyOfCell =  new CellDivide(
+                this.currRow,this.currCol,this.mass);
+        copyOfCell.direction = this.direction;
+        return copyOfCell;
+    }
+
+    /**
+     * retrieves the new position of where the cell is divided from
+     * based on the cell's direction.
+     *
+     * @return the intended position of the spawned cell
+     */
+    public int[] getDivision(){
+        switch (this.direction){
+            case DOWN:
+                direction = UP;
+                return new int[]{this.currRow+1,this.currCol};
+
+            case UP:
+                direction = LEFT;
+                return new int[]{this.currRow-1,this.currCol};
+
+            case LEFT:
+                direction = RIGHT;
+                return new int[]{this.currRow,this.currCol-1};
+
+            default:
+                direction = DOWN;
+                return new int[]{this.currRow+1,this.currCol};
+
+        }
     }
 }
