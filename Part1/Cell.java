3498beee346938b05153bb548ff3960ad0050d74
swu3025 <65374120+swu3025@users.noreply.github.com>
2020-12-31 11:58:37 -0800

    Update Cell.java

--- Part1/Cell.java
+++ Part1/Cell.java
@@ -3,11 +3,12 @@
  * ID: A16306765
  * EMAIL: saw003@ucsd.edu
  *
- * Sources used: None
+ * Sources used: https://docs.oracle.com/en/java/javase/15/docs/api/
+ * java.base/java/lang/Comparable.html
  *
  * The purpose of this file is to create a program that simulates
  * a single cell. this program is able determine the location and
- * mass of the Cell while also containing functions relating to apoptosis.
+ * mass of the Cell while also containing functions relating to the cell.
  */
 
 import java.util.List;
@@ -15,9 +16,9 @@ import java.util.List;
 /**
  * the purpose of this class is to store and obtain the information
  * of the Cell, such as the current row, column and mass of the
- * Cell while also having functions to set and check apoptosis.
+ * Cell while also having many functions to execute.
  */
-public abstract class Cell {
+public abstract class Cell implements Comparable<Cell>{
 
     public int currRow;
     public int currCol;
@@ -85,6 +86,28 @@ public abstract class Cell {
     }
 
     /**
+     * The method compares the mass of this current cell to another cell
+     * given.
+     *
+     * @param otherCell the other cell whose mass is being compared
+     * @return 1 if the current cell's mass is larger than that of the
+     * other, 2 if the other cell's mass is larger that that of the current
+     * cell, and 0 if both masses are the same
+     */
+    public int compareTo(Cell otherCell){
+//        throw new NullPointerException();
+        if(this.mass > otherCell.mass){
+            return 1;
+        }
+        else if(this.mass < otherCell.mass){
+            return -1;
+        }
+        else{
+            return 0;
+        }
+    }
+
+    /**
      * The method checks whether the current cell should initiate apoptosis
      * given a list of neighboring cell and certain conditions.
      *
@@ -92,4 +115,12 @@ public abstract class Cell {
      * @return true if the given conditions are satisfied, otherwise false
      */
     public abstract boolean checkApoptosis(List<Cell> neighbors);
+
+    /**
+     * The method creates a deep copy of the calling object, which is
+     * the current cell.
+     *
+     * @return a deep copy of the calling object
+     */
+    public abstract Cell newCellCopy();
 }
